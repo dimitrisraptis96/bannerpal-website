f8da0913364661efa17b2960f81dea84fdcd34cc
Jim Raptis <dimitrisraptis96@gmail.com>
2020-04-20 12:46:56 +0300

    :tada: First version

---
+++ hh.js
@@ -0,0 +1,8 @@
+const token = "secret";
+const title = "How to Eat Healthier";
+const author = "Eleanor Lane";
+const jobTitle = "Nutritionist";
+const background = "https://your-images.com/food";
+const template = "no1";
+
+https://api.bannerpal.com/${temlpate}/?token=${token}&title=${title}&author=${author}
